968493e46fd70abb94d1ae1b4f4a87fdb59229ed
sophie chen <pandoaki@gmial.com>
2019-07-24 18:36:42 +0800

    add else part

--- driving.py
+++ driving.py
@@ -6,7 +6,22 @@ if country == '台灣':
 		print('您可以考駕照')
 	else:
 		print('您還不能考駕照')
-elif country == '美國':
+elif country == '美國':  #根源於if,為if的延伸
+	if age >= 16:
+		print('您可以考駕照')
+	else:
+		print('您還不能考駕照')
+else:
+	print('只能輸入台灣或美國')
+country = input('請輸入您的國家：')
+age = input('請輸入您的年齡： ')
+age = int(age)
+if country == '台灣':
+	if age >= 18:
+		print('您可以考駕照')
+	else:
+		print('您還不能考駕照')
+elif country == '美國':  #根源於if,為if的延伸
 	if age >= 16:
 		print('您可以考駕照')
 	else:
